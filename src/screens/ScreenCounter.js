5fb567c88676db2fb93c4b61b76fd28986802a27
muhammadsyafiq1 <syafiqmuhammad655@gmail.com>
2023-03-09 11:15:26 +0700

    counter with reducer

--- src/screens/ScreenCounter.js
+++ src/screens/ScreenCounter.js
@@ -1,25 +1,32 @@
-import React, { useState } from "react";
+import React, { useReducer, useState } from "react";
 import {Text, View, StyleSheet, Button} from "react-native"
 
 const ScreenCounter = () => {
 
-    const [counter , setCounter] = useState(0);
+    const myFunctionReducer = (state, action) => {
+        switch (action.type){
+            case 'increment' :
+                return {...state, count: state.count + action.payload};
+            case 'decrement' :
+                return {...state, count: state.count - action.payload};
+            default:
+                return state;
+        }
+    }
+
+    const [state, dispatch] = useReducer(myFunctionReducer, {count: 0});
     
     return(
         <View>
             <Button 
                 title="Increase" 
-                onPress={() => {
-                setCounter(counter + 1)
-            }} />
+                onPress={() => dispatch({ type:'increment', payload:1 }) } />
 
             <Button
                 title="Decrease"
-                onPress={() => {
-                    setCounter(counter - 1)
-                }}
-            />
-            <Text>Score : {counter}</Text>
+                onPress={() => dispatch({ type:'decrement', payload:1 })} />
+
+            <Text>Score : {state.count}</Text>
         </View>
     );
 }
