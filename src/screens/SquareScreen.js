f33743a9044736e22a43fb006feb9c750b4eb7d1
muhammadsyafiq1 <syafiqmuhammad655@gmail.com>
2023-03-09 10:42:18 +0700

    reducer validation

--- src/screens/SquareScreen.js
+++ src/screens/SquareScreen.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useReducer, useState } from "react";
 import { View, StyleSheet } from "react-native";
 import ColorCounter from "../components/ColorCounter";
 
@@ -6,30 +6,72 @@ const SquareScreen = () => {
 
     const COLOR_INCREMENT = 15;
 
-    const [red, setRed] = useState(0);
-    const [green, setGreen] = useState(0);
-    const [blue, setBlue] = useState(0);
-    console.log(red);
+    const validateColor = (colorValue) => {
+        if(colorValue < 0 || colorValue > 255){
+            return false;
+        }
+        return true;
+    }
+
+
+    // fungsi reducer myfunction
+    const myFunction = (state, action) => {
+        //state === red:number, green:number, blue:number
+        //action === colortochange || yang akan merubah nilai state + 15
+        //memanggil dispatch dengan action sebagai argumennya
+        //colortochange adalah action
+        switch(action.colorToChange){
+            case 'red':
+              const newRed = state.red + action.amount;
+              if (validateColor(newRed)) {
+                return {...state, red: newRed};
+              } else {
+                return state;
+              }
+            case 'green':
+              const newGreen = state.green + action.amount;
+              if (validateColor(newGreen)) {
+                return {...state, green: newGreen};
+              } else {
+                return state;
+              }
+            case 'blue':
+              const newBlue = state.blue + action.amount;
+              if (validateColor(newBlue)) {
+                return {...state, blue: newBlue};
+              } else {
+                return state;
+              }
+            default:
+              return state;
+          }
+    };
+
+    const [state, dispatch] = useReducer(myFunction,{red:0, green:0, blue:0});
+
+    const {red, blue, green} = state;
+
     return(
         <View>
             <ColorCounter 
                 color="red"
-                onIncrease={() => setRed(Math.min(red + COLOR_INCREMENT, 255))}
-                onDecrease={() => setRed(Math.max(red - COLOR_INCREMENT, 0))}
+                onIncrease={() => dispatch({ colorToChange: 'red', amount: COLOR_INCREMENT })}
+                //saat fungsi dispatch ditekan akan memnaggil fungsi reducer
+                onDecrease={() => dispatch({ colorToChange: 'red', amount: -1 * COLOR_INCREMENT })}
                 /> 
 
                 <ColorCounter 
                 color="green" 
-                onIncrease={() => setGreen(Math.min(green + COLOR_INCREMENT, 255))}
-                onDecrease={() => setGreen(Math.max(green - COLOR_INCREMENT, 0))}
+                onIncrease={() => dispatch({ colorToChange: 'green', amount: COLOR_INCREMENT })}
+                onDecrease={() => dispatch({ colorToChange: 'green', amount: -1 * COLOR_INCREMENT })}
                 />
 
                 <ColorCounter 
                 color="blue" 
-                onIncrease={() => setBlue(Math.min(blue + COLOR_INCREMENT, 255))}
-                onDecrease={() => setBlue(Math.max(blue - COLOR_INCREMENT, 0))}
+                onIncrease={() => dispatch({ colorToChange: 'blue', amount: COLOR_INCREMENT })}
+                onDecrease={() => dispatch({ colorToChange: 'blue', amount: -1 * COLOR_INCREMENT })}
                 />
-                <View style={{ height:150, width:150, backgroundColor:`rgb(${red},${green},${blue})` }}></View>
+                <View style={{ height:150, width:150, backgroundColor:`rgb(${state.red},${state.green},${state.blue})` }}></View>
         </View>
     );
 }
